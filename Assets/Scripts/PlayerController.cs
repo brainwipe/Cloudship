d84d09984c02f9592bcd28ae936c94168266c816
Rob Lang <brainwiped@gmail.com>
2017-08-21 16:17:32 +0100

    Added basic assets and first try at cloudship movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rigidBody;
+    public float thrust;
+    public float turn;
+    public float velocity;
+    public float Speed = 10f;
+    public float Torque = 10f;
+    public float MaxVelocity = 0.4f;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        rigidBody.maxAngularVelocity = 0.6f;
+    }
+
+    void Update()
+    {
+        thrust = Input.GetAxis("Vertical");
+        turn = Input.GetAxis("Horizontal");
+    }
+
+    void FixedUpdate()
+    {
+        float yaw = turn * Torque * Time.deltaTime;
+        rigidBody.AddTorque(transform.up * yaw);
+
+        float forward = thrust * Speed * Time.deltaTime;
+        rigidBody.AddForce(transform.forward * forward);
+
+        velocity = rigidBody.velocity.sqrMagnitude;
+        if (velocity > MaxVelocity)
+        {
+            rigidBody.AddForce(transform.forward * (MaxVelocity - velocity));
+        }
+    }
+}
