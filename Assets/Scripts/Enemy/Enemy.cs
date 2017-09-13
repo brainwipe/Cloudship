128fda4dd8e3fd08c6366b974ee63d8944fdb6ea
Rob Lang <brainwiped@gmail.com>
2017-09-13 21:28:21 +0100

    Added Enemy
    Reordered some files. Added finite state machine with single Patrol state.

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour {
+
+	private Transform player;
+	
+	void Awake()
+	{
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+	}
+}
---
+++ Assets/Scripts/Enemy/Patrol.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : StateMachineBehaviour
+{
+
+    GameObject enemy;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        enemy = animator.gameObject;
+		enemy.transform.forward = new Vector3(
+			Random.Range(0f,1f), 
+			0f, 
+			Random.Range(0f,1f));
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        // TODO ADD WEATHER
+		enemy.transform.Translate(Vector3.forward * Time.deltaTime);
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+}
