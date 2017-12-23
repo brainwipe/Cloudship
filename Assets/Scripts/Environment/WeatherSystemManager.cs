caff31f9de8187502a604d688c1636356bf2d8da
Rob Lang <brainwiped@gmail.com>
2017-12-23 13:47:32 +0000

    Stripped out old disc weather system and replaced with a small forward force. Also removed the enemy instance.

--- Assets/Scripts/Environment/WeatherSystemManager.cs
+++ Assets/Scripts/Environment/WeatherSystemManager.cs
@@ -1,119 +1,18 @@
-﻿using System.Collections;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class WeatherSystemManager : MonoBehaviour 
+public class WeatherSystemManager : MonoBehaviour
 {
-	public WeatherSystemFactory weatherSystemFactory;
-	public float ForceMultiplier = 2f;
- 	private List<Anticyclone> weatherSystems = new List<Anticyclone>();
-	private int playArea = 100;
-	private float playAreaRadius = 0;
-	private int maximumNumberOfWeatherSystems = 50;
-	private float perlinScale = 0.3f;
-	private float perlinDenominator = 0;
-	private int weatherSystemCount = 0;
-
-	void Awake()
-	{
-		perlinDenominator = playArea * perlinScale;
-		playAreaRadius = playArea / 2;
-	}
-
-	public void GenerateWeatherSystems(Vector3 position)
+    public void GenerateWeatherSystems(Vector3 centre)
     {
-		if (weatherSystemCount > maximumNumberOfWeatherSystems)
-		{
-			return;
-		}
-
-		var startX = position.x - playAreaRadius;
-		var startZ = position.z - playAreaRadius;
-
-		var endX = position.x + playAreaRadius;
-		var endZ = position.z + playAreaRadius;
-
-        for (float x=startX; x < endX; x++)
-		{
-			for (float z=startZ; z < endZ; z++)
-			{
-				if (weatherSystemCount > maximumNumberOfWeatherSystems)
-				{
-					return;
-				}
-
-				TryToCreateNewSystem(x, z);
-			}
-		}
-		PruneDistantWeatherSystems(position);
+        
     }
 
-	public Vector3 GetCycloneForce(Vector3 airborneObject)
+    public Vector3 GetCycloneForce(Vector3 cycloneForce)
     {
-        Vector3 sum = new Vector3();
-        foreach(var weatherSystem in weatherSystems)
-        {
-            sum += weatherSystem.GetForceFor(airborneObject);
-
-        }
-        return sum * ForceMultiplier;
+        return Vector3.forward;
     }
-
-	private void PruneDistantWeatherSystems(Vector3 position)
-	{
-		var pruneList = new List<Anticyclone>();
-
-		// TODO ROLA - investigate sorting after creation loop to avoid going through this whole list
-		foreach(var weatherSystem in weatherSystems)
-		{
-			var distance = weatherSystem.transform.position - position;
-			var furthestDistanceAway = playArea + weatherSystem.radius;
-			var sqrFurthestDistanceAway = furthestDistanceAway * furthestDistanceAway;
-			if (distance.sqrMagnitude > sqrFurthestDistanceAway)
-			{
-
-				pruneList.Add(weatherSystem);
-			}
- 		}
-
-		weatherSystems = weatherSystems.Except(pruneList).ToList();
-		pruneList.ForEach(w => GameObject.Destroy(w.gameObject));
-		weatherSystemCount -= pruneList.Count();
-	}
-
-	private void TryToCreateNewSystem(float x, float z)
-	{
-		var newWeatherSystemLocation = new Vector3(x, 0f, z);
-		int perlinNoiseValue = FindPerlinNoiseValue(newWeatherSystemLocation);
-
-		var weatherSystemCreationStrategy = weatherSystemFactory.FindCreationStrategy(perlinNoiseValue);
-
-		if (!HasWeatherSystemThere(newWeatherSystemLocation, weatherSystemCreationStrategy))
-		{
-			weatherSystems.Add(
-				weatherSystemFactory.Create(newWeatherSystemLocation, weatherSystemCreationStrategy));
-			weatherSystemCount++;
-		}
-	}
-
-	private int FindPerlinNoiseValue(Vector3 worldPosition)
-	{
-		float perlinX = worldPosition.x / perlinDenominator;
-		float perlinZ = worldPosition.z / perlinDenominator;
-		return (int) (Mathf.PerlinNoise(perlinX,perlinZ) * 10);
-	}
-
-	private bool HasWeatherSystemThere(Vector3 locationToCheck, 
-		IWeatherSystemCreationStrategy weatherSystemCreationStrategy)
-	{
-		foreach(var weatherSystem in weatherSystems)
-		{
-			if (weatherSystem.IsTooCloseTo(locationToCheck, weatherSystemCreationStrategy.Radius))
-			{
-				return true;
-			}
- 		}
-		return false;
-	}	
 }
