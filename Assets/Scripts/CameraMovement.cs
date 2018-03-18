6245fbd14b2727517a5382770751429567afe79c
Rob Lang <brainwiped@gmail.com>
2018-03-18 14:04:09 +0000

    Added vertical camera movement

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -12,11 +12,11 @@ public class CameraMovement : MonoBehaviour
     [Range(0.01f, 0.4f)]
     public float SmoothFactor = 0.6f;
 
-    public bool LookAtPlayer = true;
-
-    public bool RotateAroundPlayer = true;
-
     public float RotationSpeed = 5f;
+
+    public float MaxY = 1.15f;
+    public float MinY = -0.75f;
+
 
     void Start()
     {
@@ -28,20 +28,22 @@ public class CameraMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        if (RotateAroundPlayer && Input.GetMouseButton(1))
+        if (Input.GetMouseButton(1))
         {
             Quaternion turnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * RotationSpeed, Vector3.up);
             offset = turnAngle * offset;
+
+            offset = offset - new Vector3(0, Input.GetAxis("Mouse Y") * 0.05f, 0);
         }
 
         Vector3 targetPosition = Player.position + offset;
-        var newPos = Vector3.Slerp(transform.position, targetPosition, SmoothFactor);
-        transform.position = newPos;
-        
-        if (LookAtPlayer || RotateAroundPlayer)
-        {
-           transform.LookAt(Player);
-        }
+
+        float clampY = Mathf.Clamp(targetPosition.y, MinY, MaxY);
+      
+        targetPosition = new Vector3(targetPosition.x, clampY, targetPosition.z);
+
+        transform.position = Vector3.Slerp(transform.position, targetPosition, SmoothFactor);
+        transform.LookAt(Player);
 
         CompassRose.localRotation = Quaternion.Euler(0,transform.eulerAngles.y,0);
     } 
