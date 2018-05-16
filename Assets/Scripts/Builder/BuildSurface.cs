20e25a51867e91b6b1f11b6a0e14dfc15b91f601
Rob Lang <brainwiped@gmail.com>
2018-05-16 07:55:30 +0100

    Fixed issue with select building where ray was colliding with boundary

--- Assets/Scripts/Builder/BuildSurface.cs
+++ Assets/Scripts/Builder/BuildSurface.cs
@@ -3,17 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BuildSurface : MonoBehaviour {
-
-	enum BuildMode {
-		Select,
-		Place
-	}
-
+public class BuildSurface : MonoBehaviour 
+{
 	Building selectedBuilding;
 	BuildMenu buildMenu;
 	public MeshFilter Boundary;
-	BuildMode mode;
+	public Vector3 offset = Vector3.zero;
 
 	void Awake()
 	{
@@ -21,17 +16,8 @@ public class BuildSurface : MonoBehaviour {
 		{
 			buildMenu = FindObjectOfType<BuildMenu>();
 		}
-		mode = BuildMode.Select;
 	}
 
-	void OnDisable()
-	{
-	}
-
-	void OnEnable()
-	{
-	}
-	
 	void Update()
 	{
 		MouseMove();
@@ -43,12 +29,13 @@ public class BuildSurface : MonoBehaviour {
 				Building building;
 				if (IsThereABuildingUnderMousePointer(out building))
 				{
-
 					selectedBuilding = building;
+					offset = FindOffset(selectedBuilding);
 				}
 				else
 				{
 					selectedBuilding = buildMenu.SelectedBuilding.Clone(transform);
+					offset = Vector3.zero;
 				}
 				SetBoundary(selectedBuilding);
 			}
@@ -64,6 +51,7 @@ public class BuildSurface : MonoBehaviour {
 	
 		if(Input.GetMouseButtonUp(0))
 		{
+			offset = Vector3.zero;
 			PlaceBuilding();
 		}
 	}
@@ -118,11 +106,14 @@ public class BuildSurface : MonoBehaviour {
 
 		if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
 		{
-			Debug.Log(hit.transform.tag);
 			if (hit.transform.tag == "Building")
 			{
 				building = hit.transform.GetComponentInParent<Building>();
 				return true;
+			}
+			else
+			{
+				Debug.Log(hit.transform.tag + " " + hit.transform.gameObject.name);
 			}
 			return false;
 		}
@@ -134,15 +125,39 @@ public class BuildSurface : MonoBehaviour {
 		RaycastHit hit;
 		position = Vector3.zero;
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+		Debug.DrawRay(ray.origin, ray.direction * 100000, Color.green, 1);
+
 		int layerMask = 1 << 8;
 
 		if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
 		{
 			position = hit.point;
 			return true;
-			
 		}
 		return false;
+	}
+
+	Vector3 UnfilteredMousePosition()
+	{
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+		if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+		{
+			return hit.point;
+		}
+		return Vector3.zero;
+	}
+
+	Vector3 FindOffset(Building building)
+	{
+		Vector3 mouseClickedLocation = UnfilteredMousePosition();
+		Vector3 newOffset = Vector3.zero;
+		
+		newOffset = mouseClickedLocation - building.transform.position;
+		
+		return newOffset;
 	}
 
 	void SetBoundary(Building building)
