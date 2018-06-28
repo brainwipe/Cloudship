80abe4a2fbe2e1f2d0a9509467a1eb85cf011fa6
Rob Lang <brainwiped@gmail.com>
2018-06-28 07:56:49 +0100

    Enemies spawn in front of player at a distance. Fixes #16

--- Assets/Scripts/Enemy/EnemyFactory.cs
+++ Assets/Scripts/Enemy/EnemyFactory.cs
@@ -8,9 +8,13 @@ public class EnemyFactory : MonoBehaviour
     public Enemy[] EnemyPrefabs;
     Cloudship player;
 
-    float lastSeen;
-    
-    float intervalSinceLastSeen;
+    float lastTimeWeSawAnEnemy;
+    public float MinSecsSpawn = 120f;
+    public float MaxSecsSpawn = 420f;
+    public float WidthOfSpawnCone = 60f;
+    public float MinDistance = 3000f;
+    public float MaxDistance = 4000f;
+    public float SpawnAltitude = 180f;
 
     void Start()
     {
@@ -19,11 +23,47 @@ public class EnemyFactory : MonoBehaviour
 
     void Update()
     {
-        var enemies = GetComponentsInChildren<Enemy>();
-        if (enemies.Length == 0)
+        if (IsIttimeForAnEnemy())
         {
-            var location = player.transform.position + new Vector3(1000, 0,0);
-            Instantiate(EnemyPrefabs[0], location, Quaternion.identity, transform);
+            Spawn();
         }
+    }
+
+    public void Spawn()
+    {
+        var location = FindSpawnLocation();
+        Instantiate(EnemyPrefabs[0], location, Quaternion.identity, transform);
+    }
+
+    Vector3 FindSpawnLocation()
+    {
+        var bearing = player.transform.eulerAngles.y;
+        var randomBearing = Random.Range(bearing - WidthOfSpawnCone, bearing + WidthOfSpawnCone);
+        var randomDistance = Random.Range(MinDistance, MaxDistance);
+
+        var positionWithoutY = player.transform.position + (Quaternion.AngleAxis(randomBearing, Vector3.up) * Vector3.forward * randomDistance);
+        
+        positionWithoutY.y = SpawnAltitude;
+        Debug.Log($"Spawn At: Player Bearing: {bearing}, Bearing: {randomBearing}, Distance: {randomDistance}, Position: {positionWithoutY}");
+        return positionWithoutY;
+    }
+
+    public void ResetTimer()
+    {
+        lastTimeWeSawAnEnemy = Time.time;
+    }
+
+    bool IsIttimeForAnEnemy()
+    {
+        var durationSinceLastTime = lastTimeWeSawAnEnemy - Time.time;
+
+        if (durationSinceLastTime < MinSecsSpawn)
+        {
+            return false;
+        }
+        var probability = Maths.Rescale(0,1,MinSecsSpawn, MaxSecsSpawn, durationSinceLastTime);
+        var result = Random.Range(0,1);
+        Debug.Log("Duration:" + durationSinceLastTime + " Prob:" + probability + " Result: " + result);
+        return result < probability;
     }
 }
---
+++ Assets/Scripts/Tools/UnityEditorExtensions/EnemyFactoryInspector.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(EnemyFactory))]
+public class EnemyFactoryInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        
+        EnemyFactory factory = (EnemyFactory)target;
+        if(GUILayout.Button("Spawn Enemy"))
+        {
+            factory.Spawn();
+        }
+    }
+}
