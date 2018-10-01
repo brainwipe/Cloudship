f04462ee46f27d10adc9a5533e5a9d318e974623
Rob Lang <brainwiped@gmail.com>
2018-10-01 22:20:03 +0100

    Added particles to show wind direciton. Fixes #26

---
+++ Assets/Scripts/Environment/WispyCloudTrails.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.ParticleSystem;
+
+public class WispyCloudTrails : MonoBehaviour 
+{
+	ParticleSystem wisps;
+	IWindMaker windMaker;
+	Particle[] particles;
+
+	void Start () 
+	{
+		windMaker = GameManager.Instance.WindMaker;
+		wisps = GetComponent<ParticleSystem>();
+		particles = new Particle[wisps.main.maxParticles];
+	}
+	
+	void LateUpdate() 
+	{
+		var smokeMain = wisps.main;
+		var smokeEmitter = wisps.emission;
+		var count = wisps.GetParticles(particles);
+		for(int i=0; i < count; i++)
+		{
+			particles[i].velocity = windMaker.GetCycloneForce(particles[i].position) * 0.001f;
+		}
+		wisps.SetParticles(particles, count);
+	}
+
+}
